ee9b965919eb978d3f873d9a89f8d5a34fb0c0b4
Diogotb <digogo@gmail.com>
2025-04-24 17:02:17 -0300

    24.04.25

---
+++ PersistenciaDadosFlutter/SharedPreferences/exemplo02_darkmode/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main(){
+  runApp(MaterialApp(
+    theme: ThemeData(
+      brightness: Brightness.light // define como tema claro inicial
+    ),
+    darkTheme: ThemeData(
+      brightness: Brightness.dark
+    ),
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget{
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  bool _darkMode = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _carregarPreferrencias();
+  }
+
+  void _carregarPreferrencias() async {
+    //carregar as preferencias pelo SharedPreferences
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _darkMode = prefs.getBool("darkMode")?? false;
+    });
+  }
+
+  void mudarDarkMode() async{
+    setState(() {
+      _darkMode = !_darkMode;
+    });
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool("darkMode", _darkMode);
+    _carregarPreferrencias();
+  }
+
+  //build
+  @override
+  Widget build(BuildContext context){
+    return AnimatedTheme(
+      data: _darkMode ? ThemeData.dark(): ThemeData.light(),
+      duration: Duration(microseconds: 500),//trasição suave
+      child: Scaffold(
+        appBar: AppBar(title: Text("Teste DarkMode"),),
+        body: Center(
+          child: Switch(
+            value: _darkMode, 
+            onChanged: (value) => mudarDarkMode()),
+        ),
+      ));
+  }
+}
